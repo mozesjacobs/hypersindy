51ad3f224cd669cd0f14b8ed734371a1a6d124a7
Mozes Jacobs <mjacobs@holygpu8a19405.rc.fas.harvard.edu>
2023-11-22 11:31:50 -0500

    docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -27,7 +27,7 @@ templates_path = ['_templates']
 
 # -- Options for HTML output
 
-html_theme = 'classic'
+html_theme = 'sphinx-rtd-theme'
 
 # -- Options for EPUB output
 epub_show_urls = 'footnote'
